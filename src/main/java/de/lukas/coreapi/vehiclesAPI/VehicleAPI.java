479f9cdc7f373d802517dfeb29f67424b7ccd3d1
Planet of Coding <109388773+PlanetOfCoding-real@users.noreply.github.com>
2022-12-12 18:15:19 +0100

    Update VehicleAPI.java

--- src/main/java/de/lukas/coreapi/vehiclesAPI/VehicleAPI.java
+++ src/main/java/de/lukas/coreapi/vehiclesAPI/VehicleAPI.java
@@ -2,17 +2,23 @@ package de.lukas.coreapi.vehiclesAPI;
 
 public class VehicleAPI {
 
-    public static void registerVehicle(String name, String uuid) {
+    public static void registerVehicle(String vehicleid, String uuid) {
         //** TODO Register the Vehicle in Database **//
     }
 
-    public static void unregisterVehicle(String name, String uuid) {
+    public static void unregisterVehicle(String vehicleid, String uuid) {
         //** TODO Unregister the Vehicle in Database **//
     }
 
-    public static void setVehicleOwner(String name, String uuid) {
+    public static void setVehicleOwner(String vehicleid, String uuid) {
         //** TODO Set the Owner of the Vehicle in Database **//
     }
 
+    public static void sellVehicleToDealer(String vehicleid, String uuid) {
+        //** TODO Set the Name of the Vehicle in Database **//
+    }
 
+    public static void selVehicleToPlayer(String vehicleid, String uuid) {
+        //** TODO Set the Name of the Vehicle in Database **//
+    }
 }
