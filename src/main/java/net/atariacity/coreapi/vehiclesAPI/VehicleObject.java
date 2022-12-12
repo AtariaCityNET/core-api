c16a7a8e02789b3aa9c5f158bfa88f6b122a2a61
Lukasx538 <gaming.lukas538@gmail.com>
2022-12-12 18:59:21 +0100

    VehicleAPI

---
+++ src/main/java/net/atariacity/coreapi/vehiclesAPI/VehicleObject.java
@@ -0,0 +1,23 @@
+package net.atariacity.coreapi.vehiclesAPI;
+
+import org.bukkit.entity.Player;
+
+public class VehicleObject {
+
+    public VehicleObject(Integer id) {
+
+    }
+
+    public void setOwner(Player target) {
+
+    }
+
+    public void setType(VehicleTypes vehicleTypes) {
+
+    }
+
+    public void getUUID() {
+
+    }
+
+}
