fce210618927b464bd07989a30d519b0f64fe9cb
Planet of Coding <109388773+PlanetOfCoding-real@users.noreply.github.com>
2022-12-12 18:12:11 +0100

    Update VehicleAPI.java

--- src/main/java/de/lukas/coreapi/vehiclesAPI/VehicleAPI.java
+++ src/main/java/de/lukas/coreapi/vehiclesAPI/VehicleAPI.java
@@ -5,4 +5,14 @@ public class VehicleAPI {
     public static void registerVehicle(String name, String uuid) {
         //** TODO Register the Vehicle in Database **//
     }
+
+    public static void unregisterVehicle(String name, String uuid) {
+        //** TODO Unregister the Vehicle in Database **//
+    }
+
+    public static void setVehicleOwner(String name, String uuid) {
+        //** TODO Set the Owner of the Vehicle in Database **//
+    }
+
+
 }
