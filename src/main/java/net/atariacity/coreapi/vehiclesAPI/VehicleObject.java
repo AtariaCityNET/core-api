ad60e0d1fd10a0d7ee2439bd2f91feb05c24dd84
Lukasx538 <gaming.lukas538@gmail.com>
2022-12-13 16:16:20 +0100

    Updating VehiclesAPI

--- src/main/java/net/atariacity/coreapi/vehiclesAPI/VehicleObject.java
+++ src/main/java/net/atariacity/coreapi/vehiclesAPI/VehicleObject.java
@@ -2,16 +2,24 @@ package net.atariacity.coreapi.vehiclesAPI;
 
 import net.atariacity.coreapi.CoreAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class VehicleObject {
 
-    public VehicleObject(Integer id) {
-        if(CoreAPI.getVehicleAPI.getVehicleList().containsKey(id)) {
-            Entity entity = Bukkit.getEntity(CoreAPI.getVehicleAPI.getVehicleList().get(id));
-        } else {
+    protected Entity entity;
 
+    public VehicleObject(Integer id, Location loc) {
+        if(CoreAPI.getVehicleAPI.getVehicleList().containsKey(id)) {
+            if(Bukkit.getEntity(CoreAPI.getVehicleAPI.getVehicleList().get(id)) != null) {
+                entity = Bukkit.getEntity(CoreAPI.getVehicleAPI.getVehicleList().get(id));
+            } else {
+                entity = loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
+            }
+        } else {
+            entity = loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
         }
     }
 
@@ -19,7 +27,7 @@ public class VehicleObject {
 
     }
 
-    public void setType(VehicleTypes vehicleTypes) {
+    public void setType(VehicleTypes vehicleType) {
 
     }
 
